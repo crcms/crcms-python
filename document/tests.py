01098e6f67c7040027b520a2ef0336f4c5179c16
crcms <crcms@crcms.cn>
2018-06-07 17:33:05 +0800

    base

---
+++ document/tests.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+
+
+
+class TestTest(TestCase):
+
+    def test_true(self):
+        self.assertTrue(True)
---
+++ document/urls.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+
+
+'''
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index')
+]
