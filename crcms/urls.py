df6027244227c97fd1b8ac93193abbd6c6cf24eb
crcms <crcms@crcms.cn>
2018-08-07 08:14:55 +0800

    Main module

--- crcms/urls.py
+++ crcms/urls.py
@@ -1,7 +1,7 @@
-"""crcms URL Configuration
+"""community URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
+from rest_framework import routers
+from question import views
+
+router = routers.DefaultRouter()
+router.register('questions', views.QuestionViewSet)
 
 urlpatterns = [
+    path('',include(router.urls)),
     path('admin/', admin.site.urls),
-    path('document/',include('document.urls'))
+    path('questions/', include('question.urls')),
+    # path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
