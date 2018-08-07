d4e5f1ca155fa6cbf9e7707335344f4ce397abe7
crcms <crcms@crcms.cn>
2018-08-07 17:30:04 +0800

    Route migrate

--- crcms/urls.py
+++ crcms/urls.py
@@ -15,14 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from community import views
-
-router = routers.DefaultRouter()
-router.register('questions', views.QuestionViewSet)
 
 urlpatterns = [
-    path('',include(router.urls)),
     path('admin/', admin.site.urls),
     path('communities/', include('community.urls')),
     # path(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
