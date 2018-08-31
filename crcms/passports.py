cdbff6728920427994b901d36ecdd74cffb40a5e
crcms <crcms@crcms.cn>
2018-08-31 08:43:51 +0800

    Optimize

--- crcms/passports.py
+++ crcms/passports.py
@@ -8,7 +8,6 @@
 
 from .settings import PASSPORT
 from http import client
-from urllib import parse
 import json
 
 
@@ -18,25 +17,12 @@ class Passport(object):
         pass
 
     def user(self, token: str):
-        # params = parse.urlencode(self._requestParams({'token':token}))
-
-        print(self._requestParams({'token':token}))
-        data = json.dumps(self._requestParams({'token':token}))
-        print(data)
-
-        connection = client.HTTPConnection(PASSPORT['host'],timeout=10)
-        connection.request('POST',PASSPORT['routes']['user'],data,{
-            'Accept':'application/json',
-            'Content-type':'application/json',
-        })
-        response = connection.getresponse()
-        # print(response.status,response.reason,response.read())
-        json.loads(response.read(), 'utf-8')
-        print(json.loads(response.read(),'utf-8'))#json.JSONDecoder.decode(response.read().decode('utf-8'))
-        # {
-        # return $this->rpc->call(config('foundation.passport.routes.user'), $this->requestParams(['token' = > $token]));
-        # }
-        # pass
+        response = self._http(PASSPORT['routes']['user'], 'POST', self._request_params({'token': token}))
+        if response.status == 200:
+            # json.loads(response.read().decode('utf-8')) # or
+            return json.loads(response.read(), encoding='utf-8')
+        else:
+            raise Exception("Request error")
 
     def check(self, token: str) -> bool:
         # $response = $this->rpc->call(config('foundation.passport.routes.check'), $this->requestParams(
@@ -50,10 +36,17 @@ class Passport(object):
         # return $response->getStatusCode() == = 204 | | $response->getStatusCode() == = 200;
         pass
 
-    def _requestParams(self, params: dict) -> dict:
+    def _request_params(self, params: dict) -> dict:
         # return params.update({'app_key': PASSPORT.get('key'), 'app_secret': PASSPORT.get('secret')})
-        params.update({'app_key': PASSPORT.get('key'),'app_secret': PASSPORT.get('secret')})
+        params.update({'app_key': PASSPORT.get('key'), 'app_secret': PASSPORT.get('secret')})
         return params
 
-    def _get_response(self):
-        pass
+    def _http(self, url: str, method: str = 'GET', params: dict = None) -> client.HTTPResponse:
+        connection = client.HTTPConnection(PASSPORT['host'], timeout=10)
+        connection.request(method, url, None if params is None else json.dumps(params), {
+            'Accept': 'application/json',
+            'Content-type': 'application/json',
+        })
+        # todo 如果有问题注释测试看下，暂时请求后就关闭连接
+        connection.close()
+        return connection.getresponse()
